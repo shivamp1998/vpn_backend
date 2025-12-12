63aa4f34ac99fb474477b0910d0a3699c84dbf1a
sarvesh <shivampandey98@gmail.com>
2025-12-13 00:14:47 +0530

    feat: simple server setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
 
+	"github.com/joho/godotenv"
+	server "github.com/shivamp1998/vpn_backend/internal/server"
+	pb "github.com/shivamp1998/vpn_backend/proto/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error in loading environment file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":50051"
+	}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("Error in listening to port", port)
+	}
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterUserServiceServer(grpcServer, &server.Server{})
+	reflection.Register(grpcServer)
+	fmt.Print("Server connected on port", port)
+	err = grpcServer.Serve(lis)
+
+	if err != nil {
+		log.Fatal("Error in serving the grpc server")
+	}
 }
---
+++ internal/server/grpc_server.go
@@ -0,0 +1,7 @@
+package server
+
+import pb "github.com/shivamp1998/vpn_backend/proto/gen"
+
+type Server struct {
+	pb.UnimplementedUserServiceServer
+}
